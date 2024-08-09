2067754211c4124bdacc5c0c35ef0fc0ca7e6e42
Tazateam <classwork576@gmail.com>
2024-08-09 20:55:43 +0500

    project setup

---
+++ Controllers/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace online_sms.Controllers
+{
+	public class User : Controller
+	{
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+		{
+			return View();
+		}
+
+		
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace online_sms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
