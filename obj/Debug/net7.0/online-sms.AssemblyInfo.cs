6b7b9ec940879e288fdb430c0e8bff70c0e02b09
zeeshi1233 <zk305139@gmail.com>
2024-09-03 23:14:01 +0500

    changes

--- obj/Debug/net7.0/online-sms.AssemblyInfo.cs
+++ obj/Debug/net7.0/online-sms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-sms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e4105e4b45eda15fde252abd5f5933a59674a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b3ce9b4de5cfec5f55b7fa6483f3edbcfbc8b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-sms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-sms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
