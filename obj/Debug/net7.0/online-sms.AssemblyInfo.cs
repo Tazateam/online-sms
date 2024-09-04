4ce685cbf4654fa179e2243dc5ad1224c61dc0a5
TazaTeam <classwork576@gmail.com>
2024-09-05 00:09:01 +0500

    pull from zeeshan

--- obj/Debug/net7.0/online-sms.AssemblyInfo.cs
+++ obj/Debug/net7.0/online-sms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-sms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b1b78cfd77433ecdaf0e355ba034c6bdf0cf7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-sms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-sms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
